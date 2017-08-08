2ddcb88174689c3106db2fc56637090647b3d6ca
AndreaGot <andrea.gottardi@deltainformatica.eu>
2017-08-08 17:12:45 +0200

    button arrangement, collapsible shelf for JIRA processes

--- supersede-jira-plugin/src/main/resources/js/logic-supersede-prioritization.js
+++ supersede-jira-plugin/src/main/resources/js/logic-supersede-prioritization.js
@@ -1,50 +1,42 @@
-AJS.$(document).ready(
-		function() {
-			var self = jQuery(this);
-			jQuery.ajax({
-				type : "get",
-				url : "supersede-prioritization?loadIssues=y&filter="
-						+ $('#filter-select').val(),
-				success : function(data) {
-					console.log('dom', self, data);
-					$("#export-data").html(data);
-					// self.parent().parent().remove();
-					onPageLoad();
-				},
-				error : function() {
-					console.log('error', arguments);
-				}
-			});
+AJS.$(document).ready(function() {
+	var self = jQuery(this);
+	jQuery.ajax({
+		type : "get",
+		url : "supersede-prioritization?loadIssues=y&filter=" + $('#filter-select').val(),
+		success : function(data) {
+			$("#export-data").html(data);
 			onPageLoad();
-			$(".projectElement").first().addClass('aui-nav-selected')
-					.removeClass('aui-nav');
-
-		});
+		},
+		error : function() {
+			console.log('error', arguments);
+		}
+	});
+	onPageLoad();
+});
 
 function onPageLoad() {
+	alert("setto sortable");
+	AJS.tablessortable.setTableSortable(AJS.$(".sortableTableProcesses"));
 
 	AJS.$(".icon-tooltip").tooltip();
 
-	$('#filter-select').change(
-			function() {
-				$('#filter-select').val($(this).val());
-				var self = jQuery(this);
-				jQuery.ajax({
-					type : "get",
-					url : "supersede-prioritization?loadIssues=y&filter="
-							+ $(this).val(),
-					success : function(data) {
-						console.log('dom', self, data);
-						$("#export-data").html(data);
-						// self.parent().parent().remove();
-						onPageLoad();
-					},
-					error : function() {
-						console.log('error', arguments);
-					}
-				});
-				// alert("load!");
-			});
+	$('#filter-select').change(function() {
+		$('#filter-select').val($(this).val());
+		var self = jQuery(this);
+		jQuery.ajax({
+			type : "get",
+			url : "supersede-prioritization?loadIssues=y&filter=" + $(this).val(),
+			success : function(data) {
+				console.log('dom', self, data);
+				$("#export-data").html(data);
+				onPageLoad();
+			},
+			error : function() {
+				console.log('error', arguments);
+			}
+		});
+		// alert("load!");
+	});
 
 	AJS.$("#create-process-button").click(function() {
 		AJS.dialog2("#create-process-dialog").show();
@@ -57,51 +49,51 @@ function onPageLoad() {
 		$('.procFilter').val($('#filter-select').val());
 	});
 
-	$('#processId')
-			.ready(
-					function() {
-						$('#processId')
-								.val(
-										AJS.I18n
-												.getText("supersede-prioritization-create-process-default-id.label")
-												+ new Date().getTime());
-					});
+	$('#processId').ready(function() {
+		$('#processId').val(AJS.I18n.getText("supersede-prioritization-create-process-default-id.label") + new Date().getTime());
+	});
 
-	$("#process-start-button").click(
-			function() {
-				alert($(this).closest("tr").find(".issueQuery").text());
-				$(this).closest("tr").find(".issueQuery").text();
+	$("#process-start-button").click(function() {
+		alert($(this).closest("tr").find(".issueQuery").text());
+		$(this).closest("tr").find(".issueQuery").text();
 
-				var posting = $.post(
-						"http://localhost/#/supersede-dm-app/processes/new", {
-							name : 'SS test Process'
-						});
-				posting.done(function(data) {
-					alert(data);
-				})
-			});
+		var posting = $.post("http://localhost/#/supersede-dm-app/processes/new", {
+			name : 'SS test Process'
+		});
+		posting.done(function(data) {
+			alert(data);
+		})
+	});
 
 	$("#process-save-button").click(function() {
 		alert("save");
-		// var self = jQuery(this);
-		// jQuery.ajax({
-		// type : "get",
-		// url : "supersede-prioritization?showProcesses=y",
-		// success : function(data) {
-		// console.log('dom', self, data);
-		// $("#export-data").html(data);
-		// // self.parent().parent().remove();
-		// // onPageLoad();
-		// },
-		// error : function() {
-		// console.log('error', arguments);
-		// }
-		// });
-		// alert("load!");
 	});
 
 	$('.rankingButton').click(function() {
 		$('#ranking-process-id').val($(this).attr('id'));
+	});
+
+	$(".collapsible-header").click(function() {
+		$header = $(this);
+		// getting the next element
+		$content = $header.next();
+		// open up the content needed - toggle the slide- if visible, slide up,
+		// if not slidedown.
+		$content.slideToggle(500, function() {
+			// execute this after slideToggle is done
+			// change text of header based on visibility of content div
+			$header.text(function() {
+				// change text based on condition
+				if ($content.is(":visible")) {
+					$('.collapsible-span').removeClass('glyphicon-chevron-down');
+					$('.collapsible-span').addClass('glyphicon-chevron-up');
+				} else {
+					$('.collapsible-span').removeClass('glyphicon-chevron-up');
+					$('.collapsible-span').addClass('glyphicon-chevron-down');
+				}
+			});
+		});
+
 	});
 
 }
