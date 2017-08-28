9718ce14641c6c8d5ad9ae32aada1885c064b0fc
AndreaGot <andreagot@users.noreply.github.com>
2017-08-28 14:58:42 +0200

    login nullpointer fix

--- supersede-jira-plugin/src/main/java/eu/supersede/jira/plugins/logic/LoginLogic.java
+++ supersede-jira-plugin/src/main/java/eu/supersede/jira/plugins/logic/LoginLogic.java
@@ -66,14 +66,14 @@ public class LoginLogic {
 
 	public void loadConfiguration(PluginSettings settings) {
 		PluginSettings pluginSettings = settings;
-		
+
 		String jiraUser = getCurrentUser().getUsername();
 		SupersedeLogin ssLogin = ssLoginService.getLogin(jiraUser);
-		
-		if("".equals(ssLogin.getSSUser()) || ssLogin.getSSUser() == null) {
+
+		if (ssLogin != null && ("".equals(ssLogin.getSSUser()) || ssLogin.getSSUser() == null)) {
 			ssLogin = null;
 		}
-		
+
 		serverUrl = SupersedeCfg.getConfigurationValue(pluginSettings, SupersedeCfg.KEY_HOSTNAME, SupersedeCfg.DEF_HOSTNAME);
 		username = ssLogin != null ? ssLogin.getSSUser() : SupersedeCfg.getConfigurationValue(pluginSettings, SupersedeCfg.KEY_USERNAME, SupersedeCfg.DEF_USERNAME);
 		password = ssLogin != null ? ssLogin.getSSPassword() : SupersedeCfg.getConfigurationValue(pluginSettings, SupersedeCfg.KEY_PASSWORD, SupersedeCfg.DEF_PASSWORD);
